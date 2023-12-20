13b00d52d254980bd50add45b93f317b76264946
AlimEmon0333 <alimemon0333022@gmail.com>
2023-12-20 18:30:13 +0500

    backend update

--- router/teamRoute.js
+++ router/teamRoute.js
@@ -4,6 +4,6 @@ const TeamController = require("../controller/teamController")
 
 route.post("/create", TeamController.createTeam);
 route.get("/", TeamController.getTeams)
-router.get('/:teamId/members', TeamController.getTeamMembers);
+route.get('/:teamId/members', TeamController.getTeamMembers);
 
 module.exports = route
